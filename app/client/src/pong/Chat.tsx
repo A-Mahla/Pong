3e7e1fbb1903ec753b0891f9bc7a0e6c5c250cc9
GusFiveO <augustinlorain@gmail.com>
2023-02-18 18:33:45 +0100

    able to make a connectoin with the webSocket and send a message

--- app/client/src/pong/Chat.tsx
+++ app/client/src/pong/Chat.tsx
@@ -1,5 +1,22 @@
+import { Button, TextField, FormControl} from "@mui/material"
+import { useRef, useCallback } from "react"
+import io from "socket.io-client"
+
+const socket = io.connect("http://localhost:5500")
+
 export function Chat() {
+
+	const message = useRef('')
+
+	const handleClick = useCallback(() => {
+		socket.emit('message', message.current.value)
+		console.log(message.current.value)
+	}, [message])
+
 	return (
-		<div>Chat</div>
-	)
+		<FormControl>
+			<TextField type='text' label='message' inputRef={message}/>
+			<Button onClick={handleClick}>send message</Button>
+		</FormControl>
+		)
 }
--- app/server/src/chat/chat.gateway.ts
+++ app/server/src/chat/chat.gateway.ts
@@ -1,9 +1,28 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
+@WebSocketGateway({cors : {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  transports: ['websocket', 'polling'],
+  }})
+export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect{
   @SubscribeMessage('message')
   handleMessage(client: any, payload: any): string {
+    console.log('client: ',client)
+    console.log('payload: ',payload)
     return 'Hello world!';
+  }
+
+  afterInit(server : Server): any {
+    console.log('Initialized')
+  }
+
+  handleConnection(client: Socket, ...args: any[]): any {
+    console.log(`Client connected: ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket): any {
+    console.log(`Client disconnected: ${client.id}`);
   }
 }
