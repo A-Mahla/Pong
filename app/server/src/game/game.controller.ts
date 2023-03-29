d4636ebf27ab74515989884a0c3cbcaf91399295
Sacha LAHLOU <slahlou@e1r6p6.clusters.42paris.fr>
2023-03-29 14:34:21 +0200

    game in progress ...

--- app/server/src/game/game.controller.ts
+++ app/server/src/game/game.controller.ts
@@ -107,4 +107,7 @@ export class GameController {
 		this.gameService.registerNewPlayer(parseInt(newGame.game_id.toString()), parseInt(players.player2.id), players.player2.score);
 
 	}
+
+	// the curl commande to use it :
+	// curl -X POST -H "Content-Type: application/json" -d '{"player1": {"id": "3", "score": 10}, "player2": {"id": "1", "score": 15}}' http://10.11.6.6:8080/api/game/test/createFullGame
 }
