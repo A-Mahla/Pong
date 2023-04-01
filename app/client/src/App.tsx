7e5500a42bcea174340aa2baf1d1a051bf71ff71
Amir Mahla <ammah@MacBook-Pro-de-Amir-2.local>
2023-04-01 04:48:00 +0200

    Background Ok

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -10,52 +10,54 @@ function App() {
 		const particles = Particles.init({
 			selector: '.background',
 			maxParticles: 150,
-			color: '#aeb6bf',
-			connectParticles: true,
+//			color: '#aeb6bf',
+			color: '#FFFFFF',
+//			connectParticles: true,
+			connectParticles: false,
 			speed: 0.1,
 			minDistance: 120,
-			sizeVariations: 1,
+//			sizeVariations: 1,
 			responsive: [
 				{
 					breakpoint: 1500,
 					options: {
 						maxParticles: 120,
-						minDistance: 120,
-						connectParticles: true
+//						minDistance: 120,
+//						connectParticles: true
 					}
 				}, {
 					breakpoint: 1250,
 					options: {
 						maxParticles: 100,
-						minDistance: 110,
-						connectParticles: true
+//						minDistance: 110,
+//						connectParticles: true
 					}
 				}, {
 					breakpoint: 1024,
 					options: {
 						maxParticles: 80,
-						minDistance: 100,
-						connectParticles: true
+//						minDistance: 100,
+//						connectParticles: true
 					}
 				}, {
 					breakpoint: 768,
 					options: {
 						maxParticles: 60,
-						minDistance: 100,
-						connectParticles: true
+//						minDistance: 100,
+//						connectParticles: true
 				}
 				}, {
 					breakpoint: 425,
 					options: {
 						maxParticles: 25,
-						minDistance: 100,
-						connectParticles: true
+//						minDistance: 100,
+//						connectParticles: true
 				}
 				}, {
 					breakpoint: 320,
 					options: {
-						minDistance: 40,
-						maxParticles: 10 // disables particles.js
+//						minDistance: 40,
+//						maxParticles: 10 // disables particles.js
 					}
 				}
 			]
