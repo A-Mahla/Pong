7fd2d954bcc904b86afe3b74e7106ac496cc4364
Amir Mahla <ammah@MacBook-Pro-de-Amir-2.local>
2023-04-06 01:12:45 +0200

    Search Profile in progress

--- app/server/fill_db_user_script.sh
+++ app/server/fill_db_user_script.sh
@@ -1,8 +1,8 @@
-curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"sacha", "password":"lahlou", "avatar":"slahlou.JPG"}'
+curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"sacha", "password":"lahlou", "avatar":"slahlou.jpg"}'
 
-curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"gus", "password":"alorain", "avatar":"alorain.jpeg"}'
+curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"gus", "password":"alorain", "avatar":"alorain.jpg"}'
 
-curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"amir", "password":"mahla", "avatar":"amahla.JPG"}'
+curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"amir", "password":"mahla", "avatar":"amahla.jpg"}'
 
 
 curl -i -X POST http://localhost:3500/api/auth/signup -H "Content-Type: application/json" -d '{"login":"random", "password":"user"}'
