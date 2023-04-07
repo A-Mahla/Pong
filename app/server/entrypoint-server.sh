1d901626cc8a9d57965b758c215cdacdaf06bdaf
Amir Mahla <ammah@MacBook-Pro-de-Amir-2.local>
2023-04-07 04:28:54 +0200

    Docker-Prod in progress

--- app/server/entrypoint-server.sh
+++ app/server/entrypoint-server.sh
@@ -3,8 +3,9 @@
 if [ ! -d "/app/prisma/migrations" ]
 then
 #	npx prisma migrate deploy
-#	npx prisma generate
-	node dist/main.js
+	npx prisma migrate dev --name init
+	npx prisma generate
+	npm run build && node dist/main.js
 else
 	node dist/main.js
 fi
