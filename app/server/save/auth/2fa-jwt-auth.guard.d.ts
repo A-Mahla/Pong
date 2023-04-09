1f08dd4416940306c7f153a7815bb62430e1b393
Amir Mahla <ammah@MacBook-Pro-de-Amir-2.local>
2023-04-09 04:36:22 +0200

    Docker Production done

--- app/server/save/auth/2fa-jwt-auth.guard.d.ts
+++
@@ -1,4 +0,0 @@
-declare const TwoFAJwtAuthGuard_base: import("@nestjs/passport").Type<import("@nestjs/passport").IAuthGuard>;
-export declare class TwoFAJwtAuthGuard extends TwoFAJwtAuthGuard_base {
-}
-export {};
--- app/server/save/auth/constants.d.ts
+++
@@ -1,7 +0,0 @@
-export declare const jwtConstants: {
-    jwt_secret: string | undefined;
-    refresh_jwt_name: string | undefined;
-    refresh_jwt_secret: string | undefined;
-    twofa_jwt_name: string | undefined;
-    twofa_jwt_secret: string | undefined;
-};
